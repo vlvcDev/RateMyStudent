ca5993fab71c0c4c1816877b57162ee3f71469a0
Vincent Cordova <144645040+vlvcDev@users.noreply.github.com>
2024-03-09 01:47:58 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-# RateMyStudent
+# Overview
+
+##Use this section to outline the vision for the product to be developed, including a use case diagram that shows the main user interactions with the product, in order to provide readers with an overview of the project.
+
+The 'Rate My Student' app envisions creating a transparent and collaborative educational ecosystem by providing a platform for constructive feedback and performance evaluation. Our software aims to address the challenge of subjective assessment and foster a positive learning environment for students. The primary goal is to empower educators and students themselves to contribute valuable insights into individual learning journeys. This app targets educational institutions, professors, and students. By encouraging open communication and feedback, 'Rate My Student' strives to enhance the educational experience, promote continuous improvement, and ultimately contribute to the success and well-rounded growth of every student.
+
+![](images/logo.png)
+
+# Design
+
+## User Stories
+
+Describe the user stories designed for the project, including clear acceptance criteria and point estimates for each of them. User stories must be consistent with the use case diagram. Refer to the user stories using US#1, US#2, etc. At least one of the user stories, not related to user creation or authentication, must be detailed by a sequence diagram. 
+
+### US#1
+
+*As a user, I want to register on the online platform as a student or a professor so that I can log-in. Given that a user provides a username, full name, email, and password, user information is saved when the user clicks on the "Sign Up" button, and a user profile is created. When signup is successful, an authorized user can log-in with my Username and password. If credentials are valid, I can access my reviews, search other with a search bar, view other user profiles and their ratings, create reviews, view my recommendations, and log out.*
+
+Additional Information:
+* Student sign-up includes username, name, email, and password*
+* Professor sign-up includes username, name, email, password. *
+* Professors will only display the recommendations they have created."
+
+### US#2 
+
+*As an authenticated user, from the homepage, I can utilize a search bar to search for a professor or a student. I can select a user to view the user name, their overall rating, and reviews made for other students. I can view essential details for each user such as their name, average rating, and a list of their reviews.* 
+
+Additional information:
+
+* Students will have a name, and average reviews of different criteria. *
+* Professors will have name, and a list of recommended students. *
+* reviews displayed should only show the comment and review left. Users who left the review should remain anonymous. *
+
+### US#3 
+*As a student, I can leave a single review when I access another user's review page. When I click the "Review" button, I will be directed to create a review. If the review is for a student, the user will be redirected to a form to rate students based on mastery of subject mastery, communication, participation, and leave a comment. The user will click the "submit button" when the review is complete. The student being reviewed will update to include the new review made and will display on their profile. Upon submission, individual criteria and overall average will be recalculated and updated.*
+
+Additional Information: 
+* Students will have a review form. *
+* The criteria will be on a scale of 1-5. *
+
+### US#4 
+
+*As a student, I can access a list of the reviews that my cohort has made for me. These reviews will contribute to the overall rating on my profile. I can select the review and click the "Report" button. Upon reporting, the flagged review will be made available to the admin for investigation.*
+
+Additional information: 
+* Flagged review will be added to the Admin list of flagged reports. *
+
+### US#5 
+
+*As a professor, I can recommend a student for academic opportunities. When I select a user, I can click the "recommend" button, and give a description for why I'd like to recommend the student. Upon recommendation, the student recommendation list on my profile will be updated, and a recommendation will show up on the student's profile. Students will have the ability to click on their recommendation to go to my profile.*
+
+### US#6
+
+*As an admin, I will have a list of flagged reviews for investigation on my profile. Upon review, I can choose to either remove the report, or remove the reported rating with the click of a button. Removing a rating will delete it from the profile that it was posted on.*
+
+
+## Model 
+
+At a minimum, this section should have a class diagram that succinctly describes the main classes designed for this project, as well as their associations.
+### Use Case Diagram
+
+![](images/useCaseDiagram.png)
+
+### Class Diagram
+
+![](images/umlClassDiagram.png)
+
+### Sequence Diagram
+
+![](images/sequenceDiagramUS2.png)
+
+# Development Process 
+
+This section should be used to describe how the scrum methodology was used in this project. As a suggestion, include the following table to summarize how the sprints occurred during the development of this project.
+
+|Sprint#|Goals|Start|End|Done|Observations|
+|---|---|---|---|---|---|
+|1|US#1, US#2|11/16/23|11/16/23|US#1, US#2|No issues encountered this sprint. Great communication and productivity. Intend to proceed with same course|
+|2|US#3|11/23/23|11/30/23|partial US#3|Communication slowed with fall break and illness in the group. Intending to proceed with frequent communication and intentional planning|
+|3|US#3, US#4, US#5, US#6|11/30/23|12/01/23|US#3, US#4, US#5, US#6|Had difficulties with black box testing on different OS|
+
+Use the observations column to report problems encountered during a sprint and/or to reflect on how the team has continuously improved its work.
+
+Feel free to use your own format for this section, as long as you are able to communicate what has been described here.
+
+# Testing 
+
+Share in this section the results of the tests performed to attest to the quality of the developed product, including the coverage of the tests in relation to the written code. There is no minimum code coverage expectation for your tests, other than expecting "some" coverage through at least one white-box and one black-box test.
+
+# Deployment 
+
+The final product must demonstrate the integrity of at least 5 of the 6 planned user stories. The final product must be packaged in the form of a docker image. In this section, describe the steps needed to generate that image so that others can deploy the product themselves. All files required for the deployment must be available, including the docker file, source/binary code, external package requirements, data files, images, etc. Instructions on how to create a container from the docker image with parameters such as port mapping, environment variables settings, etc., must be described (if needed). 
+
+## whitebox testing
+![](images/whiteboxtest.png)
+
+## blackbox testing
+![](images/blackboxtest.png)
+This report ran on mac OS. The system does not run fully on Mac. Sent videos. 
+
+##Run the image 
+...
+flask run -h 0.0.0.0 --port 5001
+...
+
+test web app using a browser and the link:
+http://127.0.0.1:5001
---
+++ app/routes.py
@@ -0,0 +1,279 @@
+# TODO: Implement school email verification
+
+from sqlalchemy import func
+from app import app, db, load_user, makeTestUsers
+from app.models import User, Student, Professor, Recommendation, Rating, Report, Administrator
+from app.forms import RatingForm, SignUpForm, SignInForm, ReportForm
+from flask import flash, get_flashed_messages, jsonify, render_template, redirect, url_for, request
+from app.forms import RatingForm, RecommendationForm, SignUpForm, SignInForm, ReportForm
+from flask import flash, jsonify, render_template, redirect, url_for, request
+from flask_login import login_required, login_user, logout_user, current_user
+import bcrypt
+
+@app.route('/')
+@app.route('/index')
+@app.route('/index.html')
+def index():
+    if load_user('admin') == None:
+        makeTestUsers.makeAdmin()
+        makeTestUsers.makeProfessors()
+        makeTestUsers.makeStudents()
+    return render_template('index.html')
+
+# sign-in functionality
+@app.route('/users/signin', methods=['GET', 'POST'])
+def users_signin():
+    signInForm = SignInForm()
+
+    if signInForm.validate_on_submit():
+        userID = signInForm.id.data.lower()
+        userPass = signInForm.password.data.encode('utf-8')
+
+        checkUser = load_user(userID)
+        if checkUser == None:
+             flash('No user found', 'error')
+             return redirect(url_for('users_signin'))
+
+        if bcrypt.checkpw(userPass, checkUser.password):
+            login_user(checkUser)
+            print("match")
+            print("CurrentID: " + current_user.id)
+            
+            return redirect('/users/search')
+        else:
+            flash('Incorrect Password', 'error')
+            return redirect(url_for('users_signin'))
+        
+    return render_template('signin.html', form=signInForm)
+# signup functionality
+@app.route('/users/signup', methods=['GET', 'POST'])
+def users_signup():
+    signUp = SignUpForm()
+
+    if signUp.validate_on_submit():
+        password = signUp.password.data
+        password_confirm = signUp.password_confirm.data
+
+        existing_user = load_user(signUp.id.data)
+        if existing_user:
+            flash('User already exists, Please choose a different one', 'error')
+            return redirect(url_for('users_signup'))
+        
+        #Check box handeling: If box is checked, change user type to professor
+        if signUp.professor_check.data == True:
+            userType = 'professor'
+        else: 
+            userType = 'student'
+
+        if password == password_confirm:
+            hashedPass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            newUser = User(id=signUp.id.data.lower(),
+                           full_name=signUp.full_name.data,
+                           email=signUp.email.data,
+                           password=hashedPass,
+                           type=userType)
+            db.session.add(newUser)
+            db.session.commit()
+            return redirect('/users/signin')
+        else:
+            flash("Password didn't match confirmation", 'error')
+            return redirect(url_for('users_signup'))
+        
+    return render_template('signup.html', form=signUp)
+
+# Signout functionality
+@login_required
+@app.route('/users/signout', methods=['GET', 'POST'])
+def users_signout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Student/Professor search functionality
+@login_required
+@app.route('/users/search')
+def search_page():
+    print(isinstance(current_user, Student))
+    return render_template('search.html')
+
+# Search suggestion logic for main search bar
+@app.route('/search-suggestions')
+def search_suggestions():
+    
+    query = request.args.get('q', '')
+    if query:
+        # TODO?: Change this to show all students, in CSS make the window show 5 and a scroll bar
+        users = User.query.filter(User.full_name.like(f"%{query}%")).limit(10).all()
+        suggestions = [{'name': user.full_name, 'id': user.id} for user in users]
+    else:
+        suggestions = []
+
+    return jsonify(suggestions)
+
+@login_required
+@app.route('/users/<userid>', methods=['GET', 'POST'])
+def user_profile(userid):
+    targetUser = User.query.filter_by(id=userid).first()
+
+    if not targetUser:
+        print("No user found with ID:", userid)
+        return redirect(url_for('search_page'))
+
+    if targetUser.type == 'student':
+        student = Student.query.get(userid)
+        total_rating = sum(rating.rating_overall for rating in student.ratings)
+        if student.ratings:
+            average_rating = total_rating / len(student.ratings)
+        else:
+            average_rating = 0  # Default value when there are no ratings
+
+        print(average_rating)
+        print(student.recommendations)
+
+        return render_template('student_profile.html', student=student, averagerating=average_rating, )
+    
+    elif targetUser.type == 'professor':
+        # Handle professor profile
+        professor = Professor.query.get(userid)
+        print("Professor found:", professor.full_name)
+        print(professor.recommendations)
+        return render_template('professor_profile.html', professor=professor, professor_recommendations=professor.recommendations)
+    
+    elif targetUser.type == 'administrator' and current_user.type == 'administrator':
+        return redirect(url_for('reported_ratings'))
+    else:
+        print("Unrecognized user type for ID:", userid)
+        return redirect(url_for('search_page'))
+
+@login_required
+@app.route('/rate_student/<student_id>', methods=['GET', 'POST'])
+def rate_student(student_id):
+    if student_id == current_user.id:
+
+        # Redirect or show an error if the user tries to rate themselves
+        flash('You cannot rate yourself.', 'error')
+        return redirect(url_for('user_profile', userid=student_id))
+
+    student = Student.query.get_or_404(student_id)
+    form = RatingForm()
+
+    existing_rating = Rating.query.filter_by(reviewer_id=current_user.id, student_id=student_id).first()
+    if existing_rating:
+        
+        # Redirect or show an error if the user has already rated this student
+        flash('You have already rated this student.', 'error')
+        return redirect(url_for('user_profile', userid=student_id))
+
+    if form.validate_on_submit():
+        # Retrieve form data
+        rating_participation = float(form.rating_participation.data)
+        rating_communication = float(form.rating_communication.data)
+        rating_skill = float(form.rating_skill.data)
+        description = form.description.data
+
+        # Calculate overall rating
+        rating_overall = (rating_participation + rating_communication + rating_skill) / 3
+        rating_overall = float("{:.2f}".format(rating_overall))
+
+        # Create new Rating object
+        new_rating = Rating(
+            rating_overall=rating_overall,
+            rating_participation=rating_participation,
+            rating_communication=rating_communication,
+            rating_skill=rating_skill,
+            description=description,
+            reviewer_id=current_user.id, 
+            student_id=student_id
+        )
+
+        # Add new Rating to the database
+        db.session.add(new_rating)
+        db.session.commit()
+
+        # Redirect to the student's profile page after submission
+        return redirect(url_for('user_profile', userid=student_id))
+
+    return render_template('rate_student.html', form=form, student=student)
+
+
+# Report functionality
+@login_required
+@app.route('/report_rating/<int:rating_id>', methods=['GET', 'POST'])
+def report_rating(rating_id):
+    form = ReportForm()
+
+    if form.validate_on_submit():
+        # Create a report entry in the database
+        report = Report(
+            description=form.report_description.data,
+            rating_id=rating_id,
+            reporter_id=current_user.id
+        )
+
+        db.session.add(report)
+        db.session.commit()
+        
+        # Redirect to the student's profile page after submission
+        rating = Rating.query.get(rating_id)
+        return redirect(url_for('user_profile', userid=rating.student_id))
+
+    return render_template('report_rating.html', form=form)
+
+# View reported ratings functionality
+@app.route('/admin/reported_ratings')
+def reported_ratings():
+    if current_user.type != 'administrator':
+        return redirect(url_for('search_page'))
+    reported_ratings = db.session.query(Report, Rating).join(Rating).all()
+    print(reported_ratings) 
+    return render_template('reported_ratings.html', reported_ratings=reported_ratings)
+
+#This route allows the admin to review the original reported rating
+@app.route('/admin/reported_ratings/<rating_id><report_id>')
+def review_reported_ratings(rating_id, report_id):
+    reported_rating = db.session.query(Rating).filter_by(id=rating_id).first() 
+    return render_template('review_reported_ratings.html', rating_id=rating_id, report_id=report_id, reported_rating=reported_rating)
+
+#This route handles the action the admin chose to take. Either deleting the review or removing the report
+@app.route('/admin/reported_ratings/<rating_id><report_id>/<delete>')
+def reported_ratings_action(rating_id, report_id, delete):
+    if delete == 'rating':
+        ratingToDelete = db.session.query(Rating).filter_by(id=rating_id).first()
+        deletAssociatedReport = db.session.query(Report).filter_by(id=report_id).first()
+        db.session.delete(deletAssociatedReport)
+        db.session.delete(ratingToDelete)
+        db.session.commit()
+
+    elif delete == 'report':
+        reportToDelete = db.session.query(Report).filter_by(id=report_id).first()
+        db.session.delete(reportToDelete)
+        db.session.commit()
+
+    return redirect('/admin/reported_ratings')
+
+
+# this is the route for professor are able to recommend students
+@app.route('/recommend/<student_id>', methods=['GET','POST'])
+@login_required
+def recommend_student(student_id):
+    if current_user.type != 'professor':
+        return redirect(url_for('/users/search')) 
+
+    form = RecommendationForm()
+    existing_recommendation = Recommendation.query.filter_by(professor_id=current_user.id, student_id=student_id).first()
+
+    if existing_recommendation:
+        return redirect(url_for('user_profile', userid=student_id))
+
+    if form.validate_on_submit():
+        new_recommendation = Recommendation(
+            professor_id=current_user.id, 
+            student_id=student_id, 
+            description=form.description.data
+        )
+        db.session.add(new_recommendation)
+        db.session.commit()
+        return redirect(url_for('user_profile', userid=student_id))
+
+    return render_template("recommend.html", form=form, student=Student.query.filter_by(id=student_id).first())
+
+
